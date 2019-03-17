c7be67e90763211e8287c2ce241cf9f4d6f97927
rafalcl <rafalclab@gmail.com>
2019-03-17 10:35:40 +0100

    Multiple language message support

--- src/main/java/pl/coderslab/spring01hibernatekrkw01/app/AppConfig.java
+++ src/main/java/pl/coderslab/spring01hibernatekrkw01/app/AppConfig.java
@@ -8,14 +8,17 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import pl.coderslab.spring01hibernatekrkw01.converter.StringAuthorConverter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.validation.Validator;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -52,6 +55,12 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public Validator validator() {
         return new LocalValidatorFactoryBean();
     }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("de","DE"));
+        return localeResolver; }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
